419e06992cc4af3df6f602cb5098e6a473033be0
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-11-12 11:49:07 +0330

    excercise 2 completed

--- basic-exercises.js
+++ basic-exercises.js
@@ -2,3 +2,19 @@
 var firstName = "Max";
 var age = 26;
 console.log("name", firstName, "age", age);
+// number 2
+var number1 = 2;
+if (true) {
+    var number1_1 = 4;
+}
+console.log(number1);
+var string1 = "string";
+// string1 = "another string"
+function exampleFunc() {
+    var boolean1 = true;
+    if (boolean1) {
+        var boolean1 = false;
+    }
+    console.log(boolean1);
+}
+exampleFunc();
--- basic-exercises.ts
+++ basic-exercises.ts
@@ -1,5 +1,30 @@
 // number 1
+
 const firstName: string = "Max";
 const age: number = 26;
 
 console.log("name", firstName, "age", age);
+
+// number 2
+
+let number1: number = 2;
+
+if (true) {
+  let number1: number = 4;
+}
+
+console.log(number1);
+
+const string1: string = "string";
+
+// string1 = "another string"
+
+function exampleFunc() {
+  var boolean1: boolean = true;
+  if (boolean1) {
+    var boolean1: boolean = false;
+  }
+  console.log(boolean1);
+}
+
+exampleFunc();
