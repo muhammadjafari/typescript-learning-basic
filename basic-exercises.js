6128c53dfd62ad110887d870c372ddd6052f50d9
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-11-16 10:51:14 +0330

    exercise 3 completed

--- basic-exercises.js
+++ basic-exercises.js
@@ -1,20 +1,37 @@
 // number 1
-var firstName = "Max";
-var age = 26;
-console.log("name", firstName, "age", age);
+// const firstName: string = "Max";
+// const age: number = 26;
+// console.log("name", firstName, "age", age);
 // number 2
-var number1 = 2;
-if (true) {
-    var number1_1 = 4;
-}
-console.log(number1);
-var string1 = "string";
+// let number1: number = 2;
+// if (true) {
+//   let number1: number = 4;
+// }
+// console.log(number1);
+// const string1: string = "string";
 // string1 = "another string"
-function exampleFunc() {
-    var boolean1 = true;
-    if (boolean1) {
-        var boolean1 = false;
-    }
-    console.log(boolean1);
+// function exampleFunc() {
+//   var boolean1: boolean = true;
+//   if (boolean1) {
+//     var boolean1: boolean = false;
+//   }
+//   console.log(boolean1);
+// }
+// exampleFunc();
+// number 3
+var num = 5;
+var str = "string";
+var bool = true;
+var und = undefined;
+var result = num + 2;
+console.log(result);
+var strResult = "This is a " + str;
+console.log(strResult);
+var isTrue = bool && true;
+console.log(isTrue);
+if (und == undefined) {
+    console.log("is undefind");
 }
-exampleFunc();
+else {
+    console.log("is defind");
+}
--- basic-exercises.ts
+++ basic-exercises.ts
@@ -1,30 +1,52 @@
 // number 1
 
-const firstName: string = "Max";
-const age: number = 26;
+// const firstName: string = "Max";
+// const age: number = 26;
 
-console.log("name", firstName, "age", age);
+// console.log("name", firstName, "age", age);
 
 // number 2
 
-let number1: number = 2;
+// let number1: number = 2;
 
-if (true) {
-  let number1: number = 4;
-}
+// if (true) {
+//   let number1: number = 4;
+// }
 
-console.log(number1);
+// console.log(number1);
 
-const string1: string = "string";
+// const string1: string = "string";
 
 // string1 = "another string"
 
-function exampleFunc() {
-  var boolean1: boolean = true;
-  if (boolean1) {
-    var boolean1: boolean = false;
-  }
-  console.log(boolean1);
-}
+// function exampleFunc() {
+//   var boolean1: boolean = true;
+//   if (boolean1) {
+//     var boolean1: boolean = false;
+//   }
+//   console.log(boolean1);
+// }
 
-exampleFunc();
+// exampleFunc();
+
+// number 3
+
+let num: number = 5;
+let str: string = "string";
+let bool: boolean = true;
+let und: undefined = undefined;
+
+let result = num + 2;
+console.log(result);
+
+let strResult = "This is a " + str;
+console.log(strResult);
+
+let isTrue = bool && true;
+console.log(isTrue);
+
+if (und == undefined) {
+  console.log("is undefind");
+} else {
+  console.log("is defind");
+}
